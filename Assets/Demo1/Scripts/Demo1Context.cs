88af4f57b65cac0b24a89ecd4903aea7229614c2
fanxinchen <40666638@qq.com>
2017-09-21 18:34:49 +0800

    框架更新

---
+++ Assets/Demo1/Scripts/Demo1Context.cs
@@ -0,0 +1,9 @@
+﻿using strange.extensions.context.impl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo1Context : MVCSContext {
+
+	public Demo1Context(MonoBehaviour view) : base(view) { }
+}
